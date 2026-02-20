84205c3e5909c2a4d6ab7d66bee96ba9ff1bfac3
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2026-02-20 12:05:26 +0530

    update changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.epoquegroup.in",
+          },
+        ],
+        destination: "https://epoquegroup.in/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
