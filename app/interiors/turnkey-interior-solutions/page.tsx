1f47e994a368bbd7ee405fb976cc1f835e72796f
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2026-03-02 12:00:13 +0530

    updates changes

--- app/interiors/turnkey-interior-solutions/page.tsx
+++ app/interiors/turnkey-interior-solutions/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import TurnkeyInteriorClient from "../TurnkeyInteriorClient"
+import TurnkeyInteriorClient from "./TurnkeyInteriorClient"
 
 export const metadata: Metadata = {
     title:
