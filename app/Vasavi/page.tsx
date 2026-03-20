543eef36967a8c8534759e86ccc775350329ee55
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2026-03-20 12:01:09 +0530

    first commit

--- app/Vasavi/page.tsx
+++ app/Vasavi/page.tsx
@@ -1,14 +1,12 @@
 import AboutSection from "../Project2/AboutSection";
 import AmenitiesSection from "../Project2/AmenitiesSection";
-
 import GallerySection from "../Project2/GallerySection";
 import HeroSection from "../Project2/HeroSection";
 import Navbar from "../Project2/Navbar";
 import ProjectOverview from "../Project2/ProjectOverview";
 import Specifications from "../Project2/Specifications";
 
-
-export default function Page(): JSX.Element {
+export default function Page() {
     return (
         <>
             <Navbar />
