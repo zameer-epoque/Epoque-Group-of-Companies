35696c95eba6a53410f2c5a34d4bfdb990963151
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2026-04-10 17:09:44 +0530

    first commit

--- app/components/ClientLayout.tsx
+++ app/components/ClientLayout.tsx
@@ -127,7 +127,7 @@ export default function ClientLayout({
     useEffect(() => {
         const timer = setTimeout(() => {
             setLoading(false)
-        }, 12000) // ✅ increased time for flip animation
+        }, 7000) // ✅ increased time for flip animation
 
         return () => clearTimeout(timer)
     }, [])
