79c037c6b50ed0a4f68cd5a39ce9559b1d575e67
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2026-04-21 11:26:13 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -20,31 +20,54 @@
 // module.exports = nextConfig;
 
 
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   async redirects() {
+//     return [
+//       // 1️⃣ Force www → non-www
+//       {
+//         source: "/:path*",
+//         has: [
+//           {
+//             type: "host",
+//             value: "www.epoquegroup.in",
+//           },
+//         ],
+//         destination: "https://epoquegroup.in/:path*",
+//         permanent: true,
+//       },
+//       // 2️⃣ Force HTTP → HTTPS
+//       {
+//         source: "/:path*",
+//         has: [
+//           {
+//             type: "header",
+//             key: "x-forwarded-proto",
+//             value: "http",
+//           },
+//         ],
+//         destination: "https://epoquegroup.in/:path*",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async redirects() {
     return [
-      // 1️⃣ Force www → non-www
       {
         source: "/:path*",
         has: [
           {
             type: "host",
             value: "www.epoquegroup.in",
-          },
-        ],
-        destination: "https://epoquegroup.in/:path*",
-        permanent: true,
-      },
-      // 2️⃣ Force HTTP → HTTPS
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "http",
           },
         ],
         destination: "https://epoquegroup.in/:path*",
