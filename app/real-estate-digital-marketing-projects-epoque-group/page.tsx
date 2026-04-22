426fb6438699530134af89179ee9961802106def
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2026-04-22 11:18:43 +0530

    first commit

--- app/real-estate-digital-marketing-projects-epoque-group/page.tsx
+++ app/real-estate-digital-marketing-projects-epoque-group/page.tsx
@@ -331,7 +331,7 @@ const clients = [
     image: "/garikipati-towers-real-estate-marketing-hyderabad-epoque-group.jpg",
     description:
       "Premium real estate developer offering luxury villas and apartments with world-class design, quality construction, and high investment value.",
-    link: "/GarikipatiTowers"
+    link: "/Garikipati-Towers-1.5-2-&-3-BHK-Luxury-Apartments-For-Sale-in-Hyderabad"
 
   }
   
