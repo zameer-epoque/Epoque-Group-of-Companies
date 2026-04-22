3de9fe847dc72bef701ed24a41e20459806d5fbe
saikiranyalavarthi <yalavarthisaikiran3482@gmail.com>
2026-04-22 10:37:13 +0530

    first commit

--- app/Project5/HeroSection.tsx
+++ app/Project5/HeroSection.tsx
@@ -205,7 +205,7 @@ export default function HeroSection() {
             aria-label="Book a site visit via WhatsApp"
             className="
               px-6 sm:px-8 md:px-10 
-              py-3 md:py-4 
+              py-3 md:py-3 
               text-sm sm:text-base md:text-lg
               rounded-full 
               bg-gradient-to-r from-[#5eead4] to-[#14b8a6] 
